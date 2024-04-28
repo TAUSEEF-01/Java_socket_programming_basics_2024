9cc14e76dc21d3cba92c45220b308ac88c95aa1c
TAUSEEF-01 <thesupremeemperor01@gmail.com>
2024-04-28 22:58:33 +0600

    updated!

--- client.java
+++ client.java
@@ -1,11 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class client {
     public static void main(String[] args) {
         try {
             System.out.println("Client stated...");
-            Socket sc = new Socket("localhost", 9000);
+            Socket sca = new Socket("localhost", 9000);
             System.out.println("Connection established!!! :))");
+
+            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Enter a string: ");
+            String str = userInput.readLine();
+            PrintWriter out = new PrintWriter(sca.getOutputStream(), true);
+            out.println(str);
+            BufferedReader in = new BufferedReader(new InputStreamReader(sca.getInputStream()));
+            System.out.println(in.readLine());
 
         } catch (Exception e) {
             e.printStackTrace();
--- server.java
+++ server.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,8 +11,14 @@ public class server {
             ServerSocket sc = new ServerSocket(9000);
             Socket sca = sc.accept();
             System.out.println("Connection established!!!");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(sca.getInputStream()));
+            String str = in.readLine();
+            PrintWriter out = new PrintWriter(sca.getOutputStream(), true);
+            out.println("Server says: " + str);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
+
